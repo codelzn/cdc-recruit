369e6ee00272ffda056fa408f8c1d318f11d39d5
codelzn <lzn724958732@gmail.com>
2022-12-13 09:43:01 +0900

    anime-alpha

--- src/components/Top/CanvasAnime/components/TorusAnime/index.tsx
+++ src/components/Top/CanvasAnime/components/TorusAnime/index.tsx
@@ -1,60 +1,79 @@
 import * as THREE from 'three'
-import { Torus, Sphere, Html } from '@react-three/drei'
-import { Leva, useControls } from 'leva'
 import gsap from 'gsap'
-import { useRef } from 'react'
-import { useFrame } from '@react-three/fiber'
+import { useEffect, useRef } from 'react'
 
+let torusGeometry = new THREE.TorusGeometry(0, 0.016, 15, 50)
 const blueMaterial = new THREE.MeshBasicMaterial({ color: new THREE.Color('#01A0E9') })
+const torusProps = {
+  radius: 0,
+}
 
-export default function TorusAnime() {
-  const { radius, y } = useControls({
-    radius: {
-      value: 1,
-      min: 0,
-      max: 1.2,
-      step: 0.01,
-    },
-    y: {
-      value: 0.0,
-      min: -1.1,
-      max: 1.1,
-      step: 0.01,
-    },
-  })
-  const sphereRef = useRef<THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial>>(null!)
+type Props = {
+  active?: boolean
+}
+
+export default function TorusAnime({ active = false }: Props) {
   const torusRef = useRef<THREE.Mesh<THREE.TorusGeometry, THREE.MeshBasicMaterial>>(null!)
-  const go1 = () => {
-    gsap.to(sphereRef.current.position, {
-      duration: 0.5,
+  const anime = () => {
+    const tl = gsap.timeline()
+    tl.to(torusRef.current.position, {
+      duration: 1,
       y: 1.1,
-      ease: 'power1.inOut',
+      ease: 'none',
     })
-  }
-  const go2 = () => {
-    gsap.to(torusRef.current, {
+    tl.to(torusProps, {
       radius: 1.2,
       duration: 1,
-      ease: 'power1.inOut',
-      onUpdate: (v) => {
-        console.log(v)
+      ease: 'power4.Out',
+      onUpdate: () => {
+        torusGeometry.dispose()
+        torusGeometry = null
+        torusGeometry = new THREE.TorusGeometry(torusProps.radius, 0.016, 15, 50)
+        torusRef.current.geometry = torusGeometry
       },
     })
+    tl.to(
+      torusRef.current.position,
+      {
+        duration: 1,
+        y: 0,
+        ease: 'none',
+      },
+      '-=1',
+    )
+    tl.to(torusProps, {
+      radius: 0,
+      duration: 1,
+      ease: 'power4.Out',
+      onUpdate: () => {
+        torusGeometry.dispose()
+        torusGeometry = null
+        torusGeometry = new THREE.TorusGeometry(torusProps.radius, 0.016, 15, 50)
+        torusRef.current.geometry = torusGeometry
+      },
+    })
+    tl.to(
+      torusRef.current.position,
+      {
+        duration: 1,
+        y: -1.1,
+        ease: 'none',
+      },
+      '-=1',
+    )
   }
+  useEffect(() => {
+    if (active) {
+      anime()
+    }
+  }, [active])
   return (
-    <>
-      <Html>
-        <div onClick={go1}>ANIME|ONE</div>
-        <div onClick={go2}>ANIME|TWO</div>
-      </Html>
-      {/* <Sphere material={blueMaterial} scale={0.02} position={[0, 0, 0]} ref={sphereRef} /> */}
-      <Torus
-        args={[0.0, 0.016, 15, 50]}
-        rotation-x={Math.PI / 2}
-        material={blueMaterial}
-        position={[0, 0, 0]}
-        ref={torusRef}
-      />
-    </>
+    <mesh
+      geometry={torusGeometry}
+      material={blueMaterial}
+      rotation-x={Math.PI / 2}
+      position={[0, 0, 0]}
+      ref={torusRef}
+    />
   )
 }
--- src/components/Top/CanvasAnime/index.tsx
+++ src/components/Top/CanvasAnime/index.tsx
@@ -1,18 +1,19 @@
 import * as THREE from 'three'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import Scene from './components/base/Scene'
-import { OrbitControls } from '@react-three/drei'
 import SphereAnime from './components/SphereAnime'
 import TorusAnime from './components/TorusAnime'
 import gsap from 'gsap'
 
 export default function CanvasAnime() {
+  const [torusActive, setTorusActive] = useState<boolean>(false)
   const sphereRef = useRef<THREE.Mesh<THREE.IcosahedronGeometry, THREE.ShaderMaterial>>(null!)
   const go = () => {
+    setTorusActive(true)
     gsap.to(sphereRef.current.material.uniforms.uProgress, {
       duration: 2,
-      value: 1,
-      ease: 'power1.inOut',
+      value: 0.5,
+      delay: 1,
     })
   }
   return (
@@ -20,7 +21,7 @@ export default function CanvasAnime() {
       <div onClick={go}>BUTTON</div>
       <Scene gl={{ outputEncoding: THREE.LinearEncoding }} flat camera={{ position: [0, 0, 5] }}>
         <SphereAnime ref={sphereRef} />
-        <TorusAnime />
+        <TorusAnime active={torusActive} />
       </Scene>
     </>
   )
