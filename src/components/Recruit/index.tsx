ee22f06bbb03348ecfa9871c357029131eb867de
codelzn <lzn724958732@gmail.com>
2023-01-12 16:16:17 +0900

    update

--- src/components/Recruit/index.tsx
+++ src/components/Recruit/index.tsx
@@ -1,16 +1,19 @@
 import { useTopData } from '@/store'
+import Link from 'next/link'
 
 export default function RecruitCom() {
   const recruits = useTopData((state) => state.recruits)
   return (
     <>
-      <section id='recruit' className='px-10 pt-24 pb-16 max-lg:pt-16 h-fit max-lg:px-4'>
+      <section id='recruit' className='px-10 pt-24 pb-16 2xl:px-16 max-lg:pt-16 h-fit max-lg:px-4'>
         <h2 className='text-4xl font-bold'>採用情報</h2>
         <ul className='flex flex-col px-5 py-6 mt-10 neu_glass gap-y-5'>
           {recruits.map((item, index) => (
             <li key={index} className='w-full grid grid-cols-10 max-lg:flex max-lg:flex-col'>
-              <div className='col-span-3 max-lg:font-medium max-lg:text-lg max-lg:tracking-wider'>{item.subtitle}</div>
-              <ul className='col-span-7'>
+              <div className='col-span-3 max-lg:font-medium max-lg:text-lg 2xl:text-xl max-lg:tracking-wider'>
+                {item.subtitle}
+              </div>
+              <ul className='col-span-7 2xl:text-lg 2xl:leading-loose'>
                 {item.recruitdata.length > 1 ? (
                   <>
                     {item.recruitdata.map((ctx, index) => (
@@ -36,9 +39,11 @@ export default function RecruitCom() {
             </li>
           ))}
         </ul>
-        <div className='px-10 py-2 mx-auto mt-16 text-4xl font-bold tracking-widest text-white rounded-lg max-lg:text-2xl max-lg:mt-10 w-fit bg-cdc-blue'>
+        <Link
+          href='https://job.mynavi.jp/24/pc/search/corp71482/outline.html'
+          className='block px-10 py-2 mx-auto mt-10 text-4xl font-bold tracking-widest text-white rounded-lg cursor-pointer max-lg:text-2xl max-lg:mt-10 w-fit bg-cdc-blue'>
           ENTRY
-        </div>
+        </Link>
       </section>
     </>
   )
